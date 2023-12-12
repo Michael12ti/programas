fef7c7f7067c4328ebcc893ef2ce3c8ab686952f
Michael12ti <153633061+Michael12ti@users.noreply.github.com>
2023-12-12 09:55:25 -0500

    Add files via upload

---
+++ leccion.py
@@ -0,0 +1,49 @@
+# operaciones matematicos
+# suma
+a = 8   # variable
+d = 12  # variable
+print("sumar a y d sera igual a",a+d)
+u = 12  # variable
+z = 18  # variable
+print("sumar a y d sera igual a",z+u)
+# resta
+c = 7.5  # variable
+d = 15.5  # variable
+print("recta d y c es igual a",d-c)
+f = 8.5  # variable
+x = 6.5  # variable
+print("recta f y x es igual a",f-x)
+# multiplicar
+t = 5*7  # variable
+z = 4*8  # variable
+print("multiplicar t es igual a",t)
+print("multiplicar t es igual a",z)
+# divide
+o = 23.5/ 5.5  # variable
+V = 30.5 / 4.5  # variable
+print("division o es igual a",o)
+print("division o es igual a",V)
+print(". . . . . . . . . . .. . . . . .  . . . . . . . . . . . . . . . . . . .. ")
+# variable de tipo string
+name = "Michael Steven Tiniganay Ortuno"  # guardar mi nombre con la funcion len
+print("mi nombre es",name)     # imprimir mi nombre con la funcion print
+print("los caracteres son",len(name))    #variable defini del nombre, con funcion len
+print(". . . . . . . . . . .. . . . . .  . . . . . . . . . . . . . . . . . . .. ")   #separador
+
+# conversion de tipo de  datos
+entero = 12  # variable
+Flotante = 8.4  # variable
+Flotante_a_entero = float(entero)    # cambio de tipo
+entero_a_Flotante = int(Flotante)    # cambio de tipo
+print("la conversion de la variable 1 es igual a",type(Flotante_a_entero))    # tipo de class float
+print("la conversion de la variable 2 es igual a",type(entero_a_Flotante))   # tipo de class int
+# Conversión de tipo de datos
+print("El tipo de dato original de la variable entero es", type(entero))  # tipo de class int
+print("El tipo de dato despues de la conversion Flotante es", type(Flotante_a_entero))  # tipo de class float
+
+print("El tipo de dato original de la variable Flotante es", type(float))  # tipo de class type
+print("El tipo de dato despues de la conversion a entero es",type(entero_a_Flotante)) # tipo de class int
+
+
+
+
